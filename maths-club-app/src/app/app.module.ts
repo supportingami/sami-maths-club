12c81e423b7e5694e38fb0885604e4c361899084
King-Mike <nsubugamike021@gmail.com>
2020-06-18 11:48:24 +0300

    ngx-markdown config

--- maths-club-app/src/app/app.module.ts
+++ maths-club-app/src/app/app.module.ts
@@ -2,23 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
+import { MarkdownModule } from 'ngx-markdown';
 
 import { AppComponent } from './app.component';
 import { ProblemCardComponent } from './components/problem-card/problem-card.component';
 import { ProblemsComponent } from './components/problems/problems.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProblemCardComponent,
-    ProblemsComponent
-  ],
+  declarations: [AppComponent, ProblemCardComponent, ProblemsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MarkdownModule.forRoot(),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
