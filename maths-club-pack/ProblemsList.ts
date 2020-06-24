b26343a370c2ea16da7cc8d211ed4c9000088286
Chris\Laptop <chris.m.clarke@live.co.uk>
2020-06-24 11:18:06 -0700

    add problem

--- maths-club-pack/ProblemsList.ts
+++ maths-club-pack/ProblemsList.ts
@@ -1,1 +1,1 @@
-export const ALL_PROBLEMS = [{"title":"9 Dots","slug":"9-dots"},{"title":"Buffon’s Needle","slug":"buffons-needle"},{"title":"Counting Squares","slug":"counting-squares"}]
+export const ALL_PROBLEMS = [{"title":"9 Dots","slug":"9-dots"},{"title":"Buffon’s Needle","slug":"buffons-needle"},{"title":"Counting Squares","slug":"counting-squares"},{"title":"Pell Numbers","slug":"pell-numbers"}]
