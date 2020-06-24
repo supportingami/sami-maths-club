4a37ea11a1e5c7fe1571389b1de0ce5a049d7f13
King-Mike <nsubugamike021@gmail.com>
2020-06-24 14:49:04 +0300

    Added sidedrawer nav

--- maths-club-app/src/app/app.module.ts
+++ maths-club-app/src/app/app.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { AppRoutingModule } from "./app-routing.module";
 import { MarkdownModule } from "ngx-markdown";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { ProblemCardComponent } from "./components/problem-card/problem-card.component";
 import { ProblemsListComponent } from "./components/problems-list/problems-list.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { MatCardModule } from "@angular/material/card";
-import { MatToolbarModule } from "@angular/material/toolbar";
+import { MaterialModule } from "./material.module";
 
 @NgModule({
   declarations: [AppComponent, ProblemCardComponent, ProblemsListComponent],
@@ -20,8 +20,9 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     HttpClientModule,
     MarkdownModule.forRoot(),
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatCardModule,
+
+    FormsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
