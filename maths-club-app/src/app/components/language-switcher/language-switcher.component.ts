45d153d87cc5e3a999c36428668fab323e6e707c
King-Mike <nsubugamike021@gmail.com>
2020-07-03 12:15:03 +0300

    Added language swithcer cmponent

---
+++ maths-club-app/src/app/components/language-switcher/language-switcher.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-language-switcher',
+  templateUrl: './language-switcher.component.html',
+  styleUrls: ['./language-switcher.component.scss']
+})
+export class LanguageSwitcherComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
