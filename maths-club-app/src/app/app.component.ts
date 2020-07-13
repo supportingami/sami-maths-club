1780800c3dbd0322ad8f1bda842b65c89020a876
chrismclarke <chris.m.clarke@live.co.uk>
2020-07-13 10:19:57 -0700

    tidy sidenav

--- maths-club-app/src/app/app.component.ts
+++ maths-club-app/src/app/app.component.ts
@@ -1,24 +1,11 @@
-import { Component, ChangeDetectorRef, OnDestroy } from "@angular/core";
-import { MediaMatcher } from "@angular/cdk/layout";
+import { Component, ViewEncapsulation } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
-export class AppComponent implements OnDestroy {
+export class AppComponent {
   title = "SAMI Maths Club App";
-  mobileWidthQuery: MediaQueryList;
-
-  private mobileWidthQueryListener: () => void;
-
-  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
-    this.mobileWidthQuery = media.matchMedia("(max-width: 990px)");
-    this.mobileWidthQueryListener = () => changeDetectorRef.detectChanges();
-    this.mobileWidthQuery.addListener(this.mobileWidthQueryListener);
-  }
-
-  ngOnDestroy() {
-    this.mobileWidthQuery.removeListener(this.mobileWidthQueryListener);
-  }
 }
