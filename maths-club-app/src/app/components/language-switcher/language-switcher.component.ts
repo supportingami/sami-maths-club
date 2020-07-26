65a558c4d49f84ca65856a03046612fd33df9e91
chrismclarke <chris.m.clarke@live.co.uk>
2020-07-26 16:57:44 -0700

    refactor language switcher to use subscribers

--- maths-club-app/src/app/components/language-switcher/language-switcher.component.ts
+++ maths-club-app/src/app/components/language-switcher/language-switcher.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
-import { ProblemService } from "../../services/problem.service";
-import { Router, ActivationEnd, ActivatedRoute } from "@angular/router";
+import { Router, ActivatedRoute } from "@angular/router";
+import { AppService } from "src/app/services/app.service";
+import { MatSelectChange } from "@angular/material/select";
 
 interface Language {
   value: string;
@@ -15,31 +16,34 @@ interface Language {
 export class LanguageSwitcherComponent {
   languages: Language[] = [
     { value: "en", label: "English" },
-    { value: "fr", label: "French" },
+    { value: "fr", label: "Français" },
   ];
   language: Language;
   constructor(
-    private problemService: ProblemService,
+    private appService: AppService,
     private router: Router,
     private route: ActivatedRoute
   ) {
-    router.events.subscribe((e) => {
-      if (e instanceof ActivationEnd) {
-        this.setLanguage(e.snapshot.params.lang);
+    this.appService.routeParams$.subscribe((params) => {
+      if (params.lang !== this.language?.value) {
+        this.setCurrentLanguage(params.lang);
       }
     });
   }
-  selectionChanged(e: any) {
-    const lang: Language = e.value;
-    this.router.navigate(["../", lang.value], {
+
+  selectionChanged(e: MatSelectChange) {
+    const oldLang = this.appService.routeParams$.value.lang;
+    const selected: Language = e.value;
+    const newLang = selected.value;
+    const newUrl = window.location.pathname.replace(oldLang, newLang);
+    this.router.navigate([newUrl], {
       relativeTo: this.route,
       replaceUrl: true,
     });
   }
 
-  setLanguage(languageCode: string = "en") {
+  setCurrentLanguage(languageCode: string = "en") {
     this.language = this.languages.find((l) => l.value === languageCode);
-    this.problemService.setLanguage(this.language.value);
   }
 
   compareObjects(o1: Language, o2: Language) {
