ea34d326ab502eb92fc7d103f5ef9ec5b344988a
Mike Nsubuga <nsubugamike021@gmail.com>
2020-07-28 00:31:46 +0300

    Added routerlinks to app policy

--- maths-club-app/src/app/app-routing.module.ts
+++ maths-club-app/src/app/app-routing.module.ts
@@ -4,11 +4,16 @@ import { Routes, RouterModule } from "@angular/router";
 import { ProblemsListComponent } from "./components/problems-list/problems-list.component";
 import { ProblemDetailComponent } from "./components/problem-detail/problem-detail.component";
 import { FacilitatorNoteComponent } from "./components/facilitator-note/facilitator-note.component";
+import { PrivacyPolicyComponent } from "./components/privacy-policy/privacy-policy.component";
+import { AppTermsComponent } from "./components/app-terms/app-terms.component";
 
 const routes: Routes = [
+  { path: "privacy", component: PrivacyPolicyComponent },
+  { path: "app-terms", component: AppTermsComponent },
   { path: ":lang", component: ProblemsListComponent },
   { path: ":lang/:slug", component: ProblemDetailComponent },
   { path: ":lang/:slug/notes", component: FacilitatorNoteComponent },
+
   { path: "", redirectTo: "en", pathMatch: "full" },
 ];
 
