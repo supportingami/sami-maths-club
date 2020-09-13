eea8b5f66614ce47649957f278238ee26973c50b
chrismclarke <chris.m.clarke@live.co.uk>
2020-09-13 12:43:04 -0700

    remove staging deploy target, code tidying

--- maths-club-app/functions/src/seoHost.ts
+++ maths-club-app/functions/src/seoHost.ts
@@ -13,48 +13,59 @@ import { JSDOM } from "jsdom";
         }
    ```
  * Adapted from https://medium.com/@jalalio/dynamic-og-tags-in-your-statically-firebase-hosted-polymer-app-476f18428b8b
- **/
-export const seoHost = functions.https.onRequest((req, res) => {
-  const userAgent = req.headers["user-agent"];
-  // TODO - find way to manage different indexes for different deploy targets
-  let indexHTML = fs.readFileSync("assets/index.html").toString();
+ *
+ * NOTE - only supports a single deploy project (as index needs to match that build)
+ * Workarounds attempted with http get requests but messy
+ * */
 
-  /**
-   * Only change meta if bot searching, web-app handles own seo changes in service
-   */
+export const seoHost = functions.https.onRequest(async (req, res) => {
+  const userAgent = req.headers["user-agent"];
+  const baseURL = `${req.protocol}://${req.hostname}`;
+  let indexHTML = fs.readFileSync("assets/index.html").toString();
   if (isBot(userAgent)) {
-    console.log("bot request", userAgent);
+    console.log("is-bot", userAgent);
     const path = req.path.split("/");
     const slug = path[2];
     if (slug) {
-      const allMeta: IProblemMeta[] = JSON.parse(
-        fs.readFileSync("assets/metadata.json", { encoding: "utf8" })
-      );
-      const meta = allMeta.find((m) => m.slug === slug);
-      if (meta) {
-        const baseURL = `${req.protocol}://${req.hostname}`;
-        const defaultDescription = `Here's a problem for you to try! If you get stuck there are also notes for facilitators included`;
-        const dom = new JSDOM(indexHTML);
-        let { document } = dom.window;
-        document = updateMeta(document, {
-          url: `${baseURL}${req.originalUrl}`,
-          title: `SAMI Maths Club - ${meta.title}`,
-          description: meta.description || defaultDescription,
-          // Note - could be replaced with externally hosted image (e.g. cloudinary)
-          image: `${baseURL}/assets/maths-club-pack/cover_images/jpgs/${slug}.jpg`,
-        });
-        indexHTML = dom.serialize();
-      }
+      const pathName = req.originalUrl;
+      indexHTML = updateProblemTags(slug, indexHTML, baseURL, pathName);
     }
   } else {
-    console.log("non-bot request", userAgent);
+    console.log("is-not-bot", userAgent);
   }
   res.status(200).send(indexHTML);
 });
 
+const updateProblemTags = (
+  slug: string,
+  indexHTML: string,
+  baseURL: string,
+  pathName: string
+) => {
+  const allMeta: IProblemMeta[] = JSON.parse(
+    fs.readFileSync("assets/metadata.json", { encoding: "utf8" })
+  );
+  const meta = allMeta.find((m) => m.slug === slug);
+  if (meta) {
+    const defaultDescription = `Here's a problem for you to try! If you get stuck there are also notes for facilitators included`;
+    const dom = new JSDOM(indexHTML);
+    let { document } = dom.window;
+    document = updateMeta(document, {
+      url: `${baseURL}${pathName}`,
+      title: `SAMI Maths Club - ${meta.title}`,
+      description: meta.description || defaultDescription,
+      // Note - could be replaced with externally hosted image (e.g. cloudinary)
+      image: `${baseURL}/assets/maths-club-pack/cover_images/jpgs/${slug}.jpg`,
+    });
+    return dom.serialize();
+  } else {
+    return indexHTML;
+  }
+};
+
 const isBot = (userAgent: string = "") => {
   // match most common bots, e.g. googlebot
-  return /bot|baiduspider|facebookexternalhit|crawler|spider|crawling|metainspector|whatsapp|slurp/i.test(
+  return /bot|baiduspider|facebookexternalhit|crawler|spider|crawling|metainspector|whatsapp|insomnia|slurp/i.test(
     userAgent
   );
 };
