089b37c7122d41905650ed4a009f8f2bc9defb78
Mike Nsubuga <nsubugamike021@gmail.com>
2020-10-27 23:00:05 +0300

    added problem deep link urls to share button

--- maths-club-app/src/app/components/problem-detail/problem-detail.component.ts
+++ maths-club-app/src/app/components/problem-detail/problem-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ChangeDetectorRef } from "@angular/core";
+import { Component, ViewEncapsulation, ChangeDetectorRef, OnInit } from "@angular/core";
 import { ProblemService } from "src/app/services/problem.service";
 import { fadeInOut } from "src/app/animations";
 import { Plugins } from '@capacitor/core';
@@ -11,27 +11,37 @@ const { Share } = Plugins;
   encapsulation: ViewEncapsulation.None,
   animations: [fadeInOut],
 })
-export class ProblemDetailComponent {
+export class ProblemDetailComponent implements OnInit {
   markdownReady = false;
   constructor(
     public problemService: ProblemService,
-    //private socialSharing: SocialSharing,
     private cdr: ChangeDetectorRef
   ) {}
 
+  ngOnInit(){
+    console.log("on init", this.problemService);
+  }
+
   onMarkdownReady() {
     this.markdownReady = true;
+    //console.log("on md ready", this.problemService.activeProblem$.value);
+
     // as markdown can be ready before page fully initialised complete manually trigger
     // change detection to avoid change detection errors
     this.cdr.detectChanges();
   }
 
+  
   async share(){
     await Share.share({
-      title: '15 numbers',
-      text: 'Real',
-      url: 'https://mathsclub.samicharity.co.uk/en/15-game',
+      title: this.problemService.activeProblem$.value.title,
+      text: this.problemService.activeProblem$.value.title,
+      url: `https://mathsclub.samicharity.co.uk/en/${this.problemService.activeProblem$.value.slug}`,
       dialogTitle: 'Share with buddies'
-    });
+    }).then((success)=>{
+      console.log("shared")
+    }).catch((err)=>{
+      console.log("an error occureed")
+    })
   }
 }
